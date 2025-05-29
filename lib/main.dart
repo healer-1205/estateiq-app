9fb68a3a3456a122164bbffde865feca5112c99f
Healer <crazyhealer1205@gmail.com>
2025-05-30 00:26:45 +0700

    refactor: enhance onboarding flow and error handling in main.dart; update dependencies in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,48 @@
+import 'package:estate_iq/presentation/screens/home_screen.dart';
+import 'package:estate_iq/presentation/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
 
 Future<void> _loadEnvironmentFile() async {
   try {
     await dotenv.load(fileName: '.env');
   } catch (e) {
-    print('Error loading .env file: $e');
+    debugPrint('Error loading .env file: $e');
     // Provide a fallback or handle error as needed
   }
 }
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  // Ensure that the Flutter engine is initialized before loading the environment file
+  WidgetsFlutterBinding.ensureInitialized();
+  await _loadEnvironmentFile();
+
+  // Initialize shared preferences
+  final prefs = await SharedPreferences.getInstance();
+  final showOnboarding = !(prefs.getBool('onboarding_complete') ?? false);
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => OnboardingProvider(showOnboarding, prefs),
+      child: const MainApp(),
+    ),
+  );
+}
+
+class OnboardingProvider extends ChangeNotifier {
+  bool _showOnboarding;
+  final SharedPreferences _prefs;
+
+  OnboardingProvider(this._showOnboarding, this._prefs);
+
+  bool get showOnboarding => _showOnboarding;
+
+  void completeOnboarding() {
+    _showOnboarding = false;
+    _prefs.setBool('onboarding_complete', true);
+    notifyListeners();
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -19,11 +50,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: Consumer<OnboardingProvider>(
+        builder: (context, onboardingProvider, child) {
+          if (onboardingProvider.showOnboarding) {
+            return OnboardingScreen();
+          } else {
+            return HomeScreen();
+          }
+        },
       ),
     );
   }
